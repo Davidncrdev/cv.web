1ff7ad7617b1dc206fc3d330a5ea2f7f4aadf881
David Nacro <166960840+Davidncrdev@users.noreply.github.com>
2024-09-08 01:51:36 +0200

    primer comit

---
+++ README.md
@@ -0,0 +1,30 @@
+<h2 align="center">
+  <p>David Kabre - Personal Website</p>
+  <a href="https://tupaginapersonal.com/" target="_blank">https://tupaginapersonal.com/</a>
+</h2>
+
+<div align="center">
+  <img alt="Demo" src="./demo/demo.webp" />
+  <img alt="PageSpeed Score" src="./demo/pagespeed-score.png" />
+</div>
+
+<p align="center">
+Code is licensed under MIT, words and images are licensed under <a href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>CC BY-NC-SA 4.0</a>.
+</p>
+
+## 🚀 Project Structure
+
+Inside of the Astro project for my personal marketing and communication website, you'll find the following folders and files:
+
+```text
+/
+├── public/
+│   └── favicon.svg
+├── src/
+│   ├── components/
+│   │   └── Card.astro
+│   ├── layouts/
+│   │   └── Layout.astro
+│   └── pages/
+│       └── index.astro
+└── package.json
---
+++ src/data/experience.ts
@@ -0,0 +1,64 @@
+import type { Props as ExperienceProp } from "../components/experience/props";
+
+export const EXPERIENCE: ExperienceProp[] = [
+  {
+    dates: "Junio 2023 — Marzo 2024",
+    title: "Marketing Manager",
+    company: "Royal Pianos S.L.",
+    companyUrl: "https://www.linkedin.com/company/royal-pianos/", // URL si tienes una
+    description:
+      "Desarrollé y ejecuté planes de marketing escalables. Dirigí y optimicé campañas publicitarias en redes sociales, gestioné de forma integral la página web de la empresa y actué como community manager. Responsable de diversas tareas dentro del equipo.",
+    technologies: [
+      "Marketing Digital",
+      "Gestión de Redes Sociales",
+      "Optimización Web",
+      "Community Management",
+    ],
+    logo: "/src/logos/rp.jpg", // Añade el logo si lo tienes
+  },
+  {
+    dates: "Enero 2017 — Junio 2023",
+    title: "Responsable de Marketing y Audiovisual",
+    company: "Centro de Artes Musicales de Málaga - Rockschool",
+    companyUrl: "https://www.linkedin.com/company/rslawards/", // URL si tienes una
+    description:
+      "Encargado de la gestión audiovisual del centro educativo, planificación y ejecución de estrategias de marketing, además de asesorar a los estudiantes en su proceso educativo y comercial.",
+    technologies: [
+      "Marketing Audiovisual",
+      "Gestión de Proyectos",
+      "Planificación Estratégica",
+      "Asesoramiento Comercial",
+    ],
+    logo: "/src/logos/rs.jpg", // Añade el logo si lo tienes
+  },
+  {
+    dates: "Enero 2023 — Marzo 2023",
+    title: "Asesor Comercial y Educativo",
+    company: "Five Stars Fitness",
+    companyUrl: "https://fivestarsfitness.com", // URL si tienes una
+    description:
+      "Evaluación y asistencia académica durante el curso. Gestión de financiación y administración de becas.",
+    technologies: ["Asesoría Comercial", "Gestión Financiera", "Planificación Académica"],
+    logo: "/src/logos/fs.jpg", // Añade el logo si lo tienes
+  },
+  {
+    dates: "Marzo 2023",
+    title: "Sales Assistant",
+    company: "Zara (Grupo Inditex)",
+    companyUrl: "https://www.linkedin.com/company/inditex/?originalSubdomain=es", // URL si tienes una
+    description:
+      "Gestión de pedidos online y en tienda, manejo de caja y realización de labores generales en tienda.",
+    technologies: ["Atención al Cliente", "Gestión de Inventarios", "Operaciones de Tienda"],
+    logo: "/src/logos/inditex.jpg", // Añade el logo si lo tienes
+  },
+  {
+    dates: "2020 — 2022",
+    title: "Asesor de Ventas Telefónico",
+    company: "ICCS (Campaña Jazztel)",
+    companyUrl: "https://www.linkedin.com/company/iccs---integrated-call-centre-services/", // URL si tienes una
+    description:
+      "Planificación y evaluación de estrategias de marketing para campañas telefónicas. Elaboración de propuestas comerciales.",
+    technologies: ["Ventas Telefónicas", "Marketing Estratégico", "Propuestas Comerciales"],
+    logo: "/src/logos/iccs.jpg", // Añade el logo si lo tienes
+  },
+];
